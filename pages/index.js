90be6dfa92b45eda91dda529aa8ba40498b43ad5
Jonathan Segal <jonathannsegal@gmail.com>
2021-09-02 20:26:52 -0500

    Update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,16 @@ import {
   List,
   ListIcon,
   ListItem,
+  Textarea
 } from '@chakra-ui/react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client"
+import { useState, useEffect } from 'react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
@@ -17,68 +26,92 @@ import { Footer } from '../components/Footer'
 
 import { NextSeo } from 'next-seo'
 
-const Index = () => (
-  <Container height="100vh">
-    <NextSeo
-      title="Page Meta Title"
-      description="This will be the page meta description"
-      canonical="https://www.canonicalurl.ie/"
-      openGraph={{
-        url: 'https://www.canonicalurl.ie/',
-        title: 'Open Graph Title',
-        description: 'Open Graph Description',
-        images: [
-          {
-            url: 'https://www.example.ie/og-image-01.jpg',
-            width: 800,
-            height: 600,
-            alt: 'Og Image Alt',
-          },
-          {
-            url: 'https://www.example.ie/og-image-02.jpg',
-            width: 900,
-            height: 800,
-            alt: 'Og Image Alt Second',
-          },
-          { url: 'https://www.example.ie/og-image-03.jpg' },
-          { url: 'https://www.example.ie/og-image-04.jpg' },
-        ],
-      }}
-    />
-    {/* <Header /> */}
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-      </Text>
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+const Index = (data) => (
+  <ApolloProvider client={client}>
+    <Container height="100vh">
+      <NextSeo
+        title="Page Meta Title"
+        description="This will be the page meta description"
+        canonical="https://www.canonicalurl.ie/"
+        openGraph={{
+          url: 'https://www.canonicalurl.ie/',
+          title: 'Open Graph Title',
+          description: 'Open Graph Description',
+          images: [
+            {
+              url: 'https://www.example.ie/og-image-01.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+            },
+            {
+              url: 'https://www.example.ie/og-image-02.jpg',
+              width: 900,
+              height: 800,
+              alt: 'Og Image Alt Second',
+            },
+            { url: 'https://www.example.ie/og-image-03.jpg' },
+            { url: 'https://www.example.ie/og-image-04.jpg' },
+          ],
+        }}
+      />
+      {/* <Header /> */}
+      <Hero />
+      <Main>
+        <Textarea placeholder="Here is a sample placeholder" value={data}/>
+        {/* <Text>
+          Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
+        </Text>
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
+        <List spacing={3} my={0}>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              Chakra UI <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
+              Next.js <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+        </List> */}
+      </Main>
+
+      <DarkModeSwitch />
+      <Footer>
+        <Text>Next ❤️ Chakra</Text>
+      </Footer>
+      <CTA />
+    </Container>
+  </ApolloProvider>
 )
 
-export default Index
+export const getStaticProps = async () => {
+  const data = "";
+  client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => data = result);
+  return {
+    props: { data },
+  };
+};
